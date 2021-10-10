b6aa7b090785d4d2a688f8c6650e700f20517ab8
יהונתן טי <ythee@g.jct.ac.il>
2021-10-10 09:18:46 +0300

    i change "deleteSubTree" function

--- MivneNetunim1/Tree.cpp
+++ MivneNetunim1/Tree.cpp
@@ -50,7 +50,7 @@ bool Tree::addSon(string fatherquestion, string newanswer, string newval)
 	Node* rootPtr = searchQuestion(fatherquestion,root);
 	if (rootPtr != NULL) {//if the question is in the tree
 		rootPtr->isLeaf = false; //now this root is have a son so he is not a leaf
-		Node* possiboleAns = new Node(newval); //if the next levl in the tree is answer and no more leaf
+		Node* possiboleAns = new Node(newval); //if the next level in the tree is answer and no more leaf
 		Answer* posiboleQue = new Answer(newanswer, possiboleAns);//if the user input is new levl in the tree create a new Answer type
 		rootPtr->answersList.push_back(posiboleQue);//the user input is in the list of posiibole answer of the root
 		return true;
@@ -64,5 +64,20 @@ string Tree::printToString(Node* p)
 }
 
 void Tree::deleteSubTree(string val)
-{
+{//מה שעשיתי פה זה שקיבלתי מצביע לצומת הרצויה, 
+//אם אין לו בנים אחלה סיימנו, אם יש לו בנים אז אני רץ עם איטרטור על כל הבנים שלו ושולח אותם לאותו סינון  בדיוק ברקורסיה
+//מה שגורם לי בסוף למחיקה של כל העץ - מחכה שתאשר לי
+	Node* tempPtr = searchQuestion(val,root);//find the node that have the string "val"
+	if (tempPtr->isLeaf) //if the node is no have sons
+		delete tempPtr;
+	else {
+		list<Answer*>::iterator iter = tempPtr->answersList.begin();
+		for (; iter != tempPtr->answersList.end(); iter++) {//check all the sons and remove them
+			Answer* temp = *iter;
+			deleteSubTree(temp->ans);//reqursive delete
+		}
+	}
+
+
+
 }
